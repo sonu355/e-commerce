7dc0a5bcafb8d7aab4acd4685452d67e1f2a24b9
harshal <harshalc562@gmail.com>
2024-02-06 22:57:04 +0530

    search bar added in the Appbar

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -1,31 +1,85 @@
 import React, { useEffect, useState } from 'react'
-import { Typography, AppBar, Toolbar, Grid, GridCard,Paper } from '@mui/material'
+import { Typography, AppBar, Toolbar, Grid, InputBase, styled, alpha} from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
 import LocalMallIcon from '@mui/icons-material/LocalMall';
 import ProductsCard from './ProductsCard';
 
 const Home = () => {
     const [products, setProducts] = useState([])
+
     useEffect(() => {
         fetch('https://fakestoreapi.com/products')
         .then(res => res.json())
         .then(data => setProducts(data))
     }, []);
-    // console.log(products)
+
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+          backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        color: 'white',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(1),
+          width: 'auto',
+        },
+      }));
+      
+      const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }));
+      
+      const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        width: '100%',
+        '& .MuiInputBase-input': {
+          padding: theme.spacing(1, 1, 1, 0),
+          // vertical padding + font size from searchIcon
+          paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+          transition: theme.transitions.create('width'),
+          [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+              width: '20ch',
+            },
+          },
+        },
+      }));
+
   return (
     <div>
-      <AppBar color='default' elevation={0}>
-        <Toolbar>
-            <LocalMallIcon fontSize='large'></LocalMallIcon>
-            <Typography variant='h4' color='black'>SlipCart</Typography>
-        </Toolbar>
-      </AppBar>
-      <Grid container style={{marginTop: '75px', display:'flex'}} spacing={5}>
-        {products.map(product => (
-            <Grid item key={product.id}>
-                <ProductsCard product={product} />
-            </Grid>
-        ))}
-      </Grid>
+        <AppBar color='default' elevation={0}>
+            <Toolbar>
+                <LocalMallIcon fontSize='large'></LocalMallIcon>
+                <Typography variant='h4' color='black'>SlipCart</Typography>
+            </Toolbar>
+            <Search>
+                <SearchIconWrapper>
+                <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+                />
+            </Search>
+        </AppBar>
+        <Grid container style={{marginTop: '75px', display:'flex'}} spacing={5}>
+            {products.map(product => (
+                <Grid item key={product.id}>
+                    <ProductsCard product={product} />
+                </Grid>
+            ))}
+        </Grid>
     </div>
   )
 }
--- src/component/pages/ProductsCard.js
+++ src/component/pages/ProductsCard.js
@@ -1,26 +1,50 @@
 import React from 'react'
-import { Card, CardMedia,Typography, CardContent, CardActions, Button } from '@mui/material';
+import { Card, CardMedia,Typography, CardContent, CardActions, Skeleton } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const truncateText = (text, maxLength) => {
+    if (text.length <= maxLength) {
+        return text;
+    }
+    return text.substring(0, maxLength - 3) + `...`;
+}
+
+const useStyles = makeStyles({
+    card: {
+        height: '350px', 
+        width: '250px',
+        margin: '3px',
+        boxShadow: '0px 4px 8px rgba(0, 0,0,0.1)'
+    },
+    media: {
+        height: '200px',
+        width: '200px',
+        margin: 'auto'
+    }
+})
 
 const ProductsCard = ({ product }) => {
+    const classes = useStyles()
+    const truncatedTitle = truncateText(product.title, 20)
+    const truncatedDescription = truncateText(product.description, 100);
+    
     return (
-        <Card style={{height: '450px', width: '250px'}}>
-          <CardMedia
-            sx={{ height: '200px', width: '200px' }}
-            image={product.image}
-          />
-          <CardContent>
-            <Typography variant='h6'>{product.title}</Typography>
-            <Typography variant="body2" color="text.secondary">
-                {product.description}
-            </Typography>
-            <Typography> {product.price}$</Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Share</Button>
-            <Button size="small">Learn More</Button>
-          </CardActions>
+        <Card className={classes.card}>
+             {product ? (
+                <CardMedia className={classes.media} image={product.image} />
+            ) : (
+                <Skeleton variant="rectangular" width={200} height={200} />
+            )}
+            <CardContent>
+                <Typography variant='h6'>{truncatedTitle}</Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {truncatedDescription}
+                </Typography>
+                <Typography> {product.price}$</Typography>
+            </CardContent>
+            <CardActions>
+            </CardActions>
         </Card>
       );
 }
-
 export default ProductsCard
